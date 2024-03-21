7613137d730a3102fe0f15a1575c5adc9e3a2d0c
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-03-21 09:19:02 +0000

    ex3done

--- 3-IMPLEMENT-NAIVE-BAYES-MODEL.py
+++ 3-IMPLEMENT-NAIVE-BAYES-MODEL.py
@@ -1,80 +1,50 @@
 import pandas as pd
-
-msg=pd.read_csv('naivetext.csv',names=['message','label'])
-
-print('The dimensions of the dataset',msg.shape)
-
-msg['labelnum']=msg.label.map({'pos':1,'neg':0})
-
-X=msg.message
-
-y=msg.labelnum
-
-print(X)
-
-print(y)
-
-
-
-#splitting the dataset into train and test data
-
 from sklearn.model_selection import train_test_split
-
-xtrain,xtest,ytrain,ytest=train_test_split(X,y)
-
-print ('\n The total number of Training Data :',ytrain.shape)
-
-print ('\n The total number of Test Data :',ytest.shape)
-
-
-
-#output of count vectoriser is a sparse matrix
-
 from sklearn.feature_extraction.text import CountVectorizer
-
-count_vect = CountVectorizer()
-
-xtrain_dtm = count_vect.fit_transform(xtrain)
-
-xtest_dtm=count_vect.transform(xtest)
-
-print('\n The words or Tokens in the text documents \n')
-
-print(count_vect.get_feature_names())
-
-df=pd.DataFrame(xtrain_dtm.toarray(),columns=count_vect.get_feature_names())
-
-
-
-# Training Naive Bayes (NB) classifier on training data.
-
 from sklearn.naive_bayes import MultinomialNB
-
-clf = MultinomialNB().fit(xtrain_dtm,ytrain)
-
-predicted = clf.predict(xtest_dtm)
-
-
-
-#printing accuracy, Confusion matrix, Precision and Recall
-
 from sklearn import metrics
 
-print('\n Accuracy of the classifer is',
+# Load the dataset
+msg = pd.read_csv('naivetext.csv', skiprows=1, names=['message', 'label'])
 
-      metrics.accuracy_score(ytest,predicted))
+# Print the dimensions of the dataset
+print('The dimensions of the dataset', msg.shape)
 
+# Convert labels to integers
+y = msg.label.astype('int')
+
+# Split the dataset into features and target variable
+X = msg.message
+
+# Splitting the dataset into train and test data
+xtrain, xtest, ytrain, ytest = train_test_split(X, y)
+
+# Output of count vectoriser is a sparse matrix
+count_vect = CountVectorizer()
+
+# Transform the training and test data using count vectorizer object
+xtrain_dtm = count_vect.fit_transform(xtrain)
+xtest_dtm = count_vect.transform(xtest)
+
+# Training Naive Bayes (NB) classifier on training data
+clf = MultinomialNB().fit(xtrain_dtm, ytrain)
+
+# Convert predicted labels to integers
+predicted = clf.predict(xtest_dtm).astype('int')
+
+# Printing accuracy, Confusion matrix, Precision and Recall
+print(X)
+print(y)
+print('The total number of Training Data:', xtrain.shape)
+print('The total number of Test Data:', xtest.shape)
+print('The words or Tokens in the text documents')
+print(count_vect.get_feature_names_out())
+
+print('\n Accuracy of the classifier is', metrics.accuracy_score(ytest, predicted))
 print('\n Confusion matrix')
-
-print(metrics.confusion_matrix(ytest,predicted)) 
-
-print('\n The value of Precision' ,
-
- metrics.precision_score(ytest,predicted)) 
-
-print('\n The value of Recall') ,
-
-metrics.recall_score(ytest,predicted)
+print(metrics.confusion_matrix(ytest, predicted)) 
+print('\n The value of Precision', metrics.precision_score(ytest, predicted)) 
+print('\n The value of Recall', metrics.recall_score(ytest, predicted))
 
 # OUTPUT
 # The dimensions of the dataset (18, 2)
